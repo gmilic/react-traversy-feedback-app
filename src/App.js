931b982b37de0c390a776bbd4d968e6f5c810cda
Goran Milic <gmilic@gmail.com>
2022-05-06 10:07:11 +0200

    Lesson 19

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 import FeedbackData from './data/FedbackData'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure?')) {
+      setFeedback(
+        feedback.filter((item) => {
+          return item.id !== id
+        })
+      )
+    }
+  }
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   )
